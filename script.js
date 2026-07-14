fe113f760e03abf54b8838ee2bb6e30f2c627f9d
unknown <odewoyeoyindamola25@gmail.com>
2026-07-14 14:48:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function runTest(){
+
+document.getElementById("wifi").innerHTML =
+"<span class='success'>✔ WPA3 Encryption Successfully Enabled</span>";
+
+document.getElementById("segment").innerHTML =
+"<span class='success'>✔ Main Network, Guest Network and IoT Network Successfully Segmented</span>";
+
+document.getElementById("auth").innerHTML =
+"<span class='success'>✔ Unauthorized Users Blocked</span>";
+
+document.getElementById("security").innerHTML =
+"<span class='success'>Network Security Level : HIGH</span>";
+
+}
